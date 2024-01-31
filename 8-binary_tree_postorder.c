8a7de4e8ea13859fa19966749ac87889d39654f3
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-01-31 21:13:02 +0200

    c

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Travtraversal.
+ * @tree: Apointer
+ * @func: Apointer
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
+}
