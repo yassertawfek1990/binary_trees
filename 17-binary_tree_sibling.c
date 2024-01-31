8dc94586395d7af556f3cfb7d34e9a9cacd62a3a
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-02-01 00:03:12 +0200

    c

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Findsnodee.
+ * @node: Apointeriblingof.
+ * Return: Ifno
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	return (node->parent->left);
+}
