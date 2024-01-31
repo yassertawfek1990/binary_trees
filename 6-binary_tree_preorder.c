68990d90a6551ee97eeb86e0815ea5019bd40339
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-01-31 21:10:32 +0200

    c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Goespre-ordertraversal.
+ * @tree: Aponter
+ * @func: Apinter
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
